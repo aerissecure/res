6898df067de5c07ac4fc06495ffa8a51c8e628ae
Jeffrey Stiles <jeff@aerissecure.com>
2016-03-18 20:41:48 -0700

    Add source code and update REAME

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # res
 Parallel hostname resolver with flexible output
+
+# Examples
+
+Normal usage:
+
+~~~
+> $ res yahoo.com google.com bing.com
+yahoo.com
+   98.139.183.24
+   98.138.253.109
+   206.190.36.45
+   2001:4998:c:a06::2:4008
+   2001:4998:44:204::a7
+   2001:4998:58:c02::a9
+
+bing.com
+   204.79.197.200
+
+google.com
+   216.58.193.174
+   2607:f8b0:400a:800::200e
+~~~
+
+Filter for only IPv4 addresses:
+
+~~~
+> $ res -4 yahoo.com google.com bing.com
+google.com
+   216.58.216.142
+
+bing.com
+   204.79.197.200
+
+yahoo.com
+   98.138.253.109
+   98.139.183.24
+   206.190.36.45
+~~~
+
+Output to a single line with space separated addresses:
+
+~~~
+> $ res -l -4 yahoo.com google.com bing.com
+204.79.197.200 98.138.253.109 98.139.183.24 206.190.36.45 216.58.216.142
+~~~
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"flag"
+	"os"
+	"strings"
+)
+
+func main() {
+	ipv4 := flag.Bool("4", false, "ipv4 addresses only")
+	ipv6 := flag.Bool("6", false, "ipv6 addresses only")
+	l := flag.Bool("l", false, "list ips")
+	flag.Parse()
+	if len(flag.Args()) == 0 {
+		fmt.Println("no hostnames specified")
+		os.Exit(1)
+	}
+	if *ipv4 && *ipv6 {
+		fmt.Println("use -4 or -6, not both")
+	}
+
+	hosts := flag.Args()
+	lookups := make(chan map[string][]net.IP)
+
+	for _, h := range hosts {
+		go func(h string) {
+			m := make(map[string][]net.IP)
+			m[h] = []net.IP{}
+			ips, _ := net.LookupHost(h)
+			for _, ip := range ips {
+				m[h] = append(m[h], net.ParseIP(ip))
+			}
+			lookups <- m
+		}(h)
+	}
+
+	hostMap := make(map[string][]net.IP)
+	for i := 0; i < len(hosts); i++ {
+		m := <- lookups
+		for h, ips := range m {
+			hostMap[h] = ips
+		}
+	}
+
+	if *l {
+		out := []net.IP{}
+		for _, ips := range hostMap {
+			for _, ip := range ips {
+				if !*ipv4 && !*ipv6 {
+					out = append(out, ip)
+				} else if *ipv4 && ip.To4() != nil {
+					out = append(out, ip)
+				} else if *ipv6 && ip.To4() == nil {
+					out = append(out, ip)
+				}
+			}
+		}
+		o := []string{}
+		for _, ip := range out {
+			o = append(o, ip.String())
+		}
+		fmt.Println(strings.Join(o, " "))
+	} else {
+		for h, ips := range hostMap {
+			fmt.Println(h)
+			for _, ip := range ips {
+				if !*ipv4 && !*ipv6 {
+					fmt.Println("  ", ip.String())
+				} else if *ipv4 && ip.To4() != nil {
+					fmt.Println("  ", ip.String())
+				} else if *ipv6 && ip.To4() == nil {
+					fmt.Println("  ", ip.String())
+				}
+			}
+			fmt.Println()
+		}
+	}
+}
